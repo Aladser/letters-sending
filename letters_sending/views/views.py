ea1dfc66a8e8cdac145a2fe9edc5f52de053ef1d
Aladser <Aladser@users.noreply.github.com>
2024-08-09 19:11:00 +0900

    обновлена верстка базового шаблона страниц

--- letters_sending/views/views.py
+++ letters_sending/views/views.py
@@ -7,11 +7,16 @@ from config.settings import APP_NAME
 from letters_sending.models import Attempt, LettersSending
 
 
+# СПИСОК ПОПЫТОК
 class AttemptListView(CustomLoginRequiredMixin, ListView):
-    """LIST"""
-
     model = Attempt
     template_name = "attempt_list.html"
+
+    title = "Статистика рассылок"
+    extra_context = {
+        'title': title,
+        'header': title,
+    }
 
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset(*args, **kwargs)
@@ -20,7 +25,6 @@ class AttemptListView(CustomLoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['header'] = "статистика отправки"
 
         for attempt in context['object_list']:
             sending = LettersSending.objects.get(pk=attempt['letters_sending'])
@@ -30,5 +34,6 @@ class AttemptListView(CustomLoginRequiredMixin, ListView):
         return context
 
 
+# ГЛАВНАЯ СТРАНИЦА
 def index_page(request):
     return render(request, 'index.html', {'header':APP_NAME})
